b196655f5b6275e96aa4554d70d915f48826c3a8
renly261 <sam410511287@gmail.com>
2021-05-10 22:52:09 +0800

    作業

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ bot.on('message', async event => {
   // 若你傳的東西類型是文字
   if (event.message.type === 'text') {
     let msg = event.message.text
-    // 若你傳送訊息的字是 !help
+    // 若你傳送的字是 !help-----------------------------------------------------------------------------
     if (msg === '!help') {
       const help =
-        '歡迎使用動畫瘋查詢機器人\n機器人指令=>\n☆!help:顯示幫助訊息\n☆!animed:空格後接關鍵字 搜尋\n例如:!animed 巨人\n\n動畫瘋自2021年1月1日起\n停止支援手機網頁撥放\n請下載動畫瘋 app 使用喔!\n一次搜尋的結果最多 12 筆\n請搜尋關鍵字時多加留意'
+        '歡迎使用動畫瘋查詢機器人\n機器人指令=>\n☆!help:顯示幫助訊息\n☆!animed:空格後接關鍵字 搜尋\n例如:!animed 巨人\n☆!today:可以顯示今日更新的動畫\n\n動畫瘋自2021年1月1日起\n停止支援手機網頁撥放\n請下載動畫瘋 app 使用喔!\n一次搜尋的結果最多 12 筆\n請搜尋關鍵字時多加留意'
       event.reply(help)
 
-      // 若你傳送的字是 !anime 關鍵字
+      // 若你傳送的字是 !anime 關鍵字--------------------------------------------------------------------------
       // msg.substring(0, 7) msg 前七個字是 !anime 會產生一個新字串 此時的 msg 會變成 !anime
     } else if (msg.substring(0, 7) === '!anime ') {
       // 判斷完 msg 後要把 !anime 替換成空的字(刪掉) 不然底下的網址會多 !anime
@@ -45,32 +45,32 @@ bot.on('message', async event => {
         const links = []
 
         // 用陣列存取 function 裡的資料就可以帶到外面
-        const arr1 = []
-        const arr2 = []
-        const arr3 = []
-        const arr4 = []
-        let arr5 = []
-        let arr6 = []
-        let arr7 = []
+        const arrHttp = []
+        const arrImg = []
+        const arrName = []
+        const arrDate = []
+        let arrType = []
+        let arrCom = []
+        let arrScore = []
 
         // 第一層---------------------------------------------------------------------------------------------------------
         // 搜尋結果的網站的資料 .old_list 裡面的 .theme-list-block 裡面的 a 標籤 全部
         $('.old_list .theme-list-block a').each(function () {
           // 搜尋結果的每個動畫網址 抓全部 a 標籤裡圖片的 href 屬性
           // console.log('https://ani.gamer.com.tw/' + $(this).attr('href'))
-          arr1.push('https://ani.gamer.com.tw/' + $(this).attr('href'))
+          arrHttp.push('https://ani.gamer.com.tw/' + $(this).attr('href'))
 
           // 搜尋結果的每個動畫圖片 抓全部 a 標籤裡圖片的 src 屬性
           // console.log($(this).find('.theme-img').attr('src'))
-          arr2.push($(this).find('.theme-img').attr('src'))
+          arrImg.push($(this).find('.theme-img').attr('src'))
           // 搜尋結果的每個動畫名稱 抓全部 a 標籤裡的名稱
           // console.log($(this).find('.theme-name').text())
-          arr3.push($(this).find('.theme-name').text())
+          arrName.push($(this).find('.theme-name').text())
 
           // 搜尋結果的每個動畫日期 抓全部 a 標籤裡的日期
           // console.log($(this).find('.theme-time').text()
           // substring( , ) 只取第幾個到第幾個字
-          arr4.push($(this).find('.theme-time').text().substring(3, 100))
+          arrDate.push($(this).find('.theme-time').text().substring(3, 100))
 
           // 抓第一層搜尋結果的每個動畫網址 塞進 links 的空陣列
           links.push('https://ani.gamer.com.tw/' + $(this).attr('href'))
@@ -89,48 +89,33 @@ bot.on('message', async event => {
             // 作品類型
             // console.log($(this).find('.data_type li').eq(0).text())
             // substring( , ) 只取第幾個到第幾個字
-            arr5.push($(this).find('.data_type li').eq(0).text().substring(4, 100))
+            arrType.push($(this).find('.data_type li').eq(0).text().substring(4, 100))
 
             // 製作廠商
             // console.log($(this).find('.data_type li').eq(4).text())
             // substring( , ) 只取第幾個到第幾個字
-            arr6.push($(this).find('.data_type li').eq(4).text().substring(4, 100))
+            arrCom.push($(this).find('.data_type li').eq(4).text().substring(4, 100))
 
             // 動畫評分
             // console.log($(this).find('.data_acgbox'))
             // 因為 span 包在 div 裡面 用選擇器抓不到 所以先 remove div 的下一層 span 在抓 div
             $('.ACG-score').children().remove()
-            arr7.push($(this).find('.ACG-score').text())
+            arrScore.push($(this).find('.ACG-score').text())
           })
         }
-
-        // 抓本季新番 今天更新的動畫
-        const response1 = await axios.get('https://ani.gamer.com.tw/')
-        const $1 = cheerio.load(response1.data)
-        $1('.newanime-block').each(function () {
-          for (let i = 1; i < 10; i++) {
-            // 動畫網址
-            $1(this).find(` .new-count-${i} .anime-card-block`).attr('href')
-            console.log('https://ani.gamer.com.tw/' + $1(this).find(` .new-count-${i} .anime-card-block`).attr('href'))
-
-            $1(this).find('.lazyloaded').attr('src')
-            // console.log($1(this).find('.lazyloaded').attr('src'))
-          }
-        })
-
         // 作品類型 消除陣列裡的空白值
         // 找陣列中有值的才 retrun 出來
-        arr5 = arr5.filter(function (a) {
+        arrType = arrType.filter(function (a) {
           return a.length > 0
         })
 
         // 製作廠商 消除陣列裡的空白值
-        arr6 = arr6.filter(function (a) {
+        arrCom = arrCom.filter(function (a) {
           return a.length > 0
         })
 
         // 動畫評分 消除陣列裡的空白值
-        arr7 = arr7.filter(function (a) {
+        arrScore = arrScore.filter(function (a) {
           return a.length > 0
         })
 
@@ -144,22 +129,22 @@ bot.on('message', async event => {
           }
         }
 
-        // 跑迴圈將搜尋結果的每個動畫資料陣列 push 進 flex message 的 contents 裡面-----------------------------------------------------------
-        for (let i = 0; i < arr1.length; i++) {
+        // 跑迴圈將搜尋結果的每個動畫資料陣列 push 進 flex message 的 contents 裡面
+        for (let i = 0; i < arrName.length; i++) {
           flex.contents.contents.push({
             // 動畫圖片
             type: 'bubble',
             size: 'micro',
             hero: {
               type: 'image',
-              url: `${arr2[i]}`,
+              url: `${arrImg[i]}`,
               size: 'full',
               aspectMode: 'cover',
               aspectRatio: '1:1',
               // 點擊圖片跳到該動畫瘋網址
               action: {
                 type: 'uri',
-                uri: `${arr1[i]}`
+                uri: `${arrHttp[i]}`
               }
             },
 
@@ -170,7 +155,7 @@ bot.on('message', async event => {
               contents: [
                 {
                   type: 'text',
-                  text: `${arr3[i]}`,
+                  text: `${arrName[i]}`,
                   weight: 'bold',
                   size: '14px',
                   wrap: true
@@ -194,7 +179,7 @@ bot.on('message', async event => {
                         },
                         {
                           type: 'text',
-                          text: `${arr7[i]}`,
+                          text: `${arrScore[i]}`,
                           size: '14px',
                           weight: 'bold',
                           margin: 'sm',
@@ -227,7 +212,7 @@ bot.on('message', async event => {
                         },
                         {
                           type: 'text',
-                          text: `${arr4[i]}`,
+                          text: `${arrDate[i]}`,
                           wrap: true,
                           size: '12px',
                           offsetStart: '5px'
@@ -250,7 +235,7 @@ bot.on('message', async event => {
                         },
                         {
                           type: 'text',
-                          text: `${arr5[i]}`,
+                          text: `${arrType[i]}`,
                           wrap: true,
                           size: '12px',
                           offsetStart: '5px'
@@ -273,7 +258,7 @@ bot.on('message', async event => {
                         },
                         {
                           type: 'text',
-                          text: `${arr6[i]}`,
+                          text: `${arrCom[i]}`,
                           wrap: true,
                           size: '12px',
                           offsetStart: '5px'
@@ -288,17 +273,252 @@ bot.on('message', async event => {
             }
           })
         }
-
-        // 若查出來的資料筆數為 0
-        if (arr1.length === 0) {
-          event.reply('搜尋不到符合此關鍵字的動畫 請在確認')
+        if (arrName.length === 0) {
+          event.reply('查不到此關鍵字的資料\n請重新確認')
         }
-
         // 若查出來有東西 linebot 回傳 flex
         event.reply(flex)
-        // console.log(arr3)
-
         // 發生錯誤要回傳的東西
+      } catch (error) {
+        event.reply('發生錯誤')
+      }
+      // 若你搜尋的是 !today-----------------------------------------------------------------------------
+      // 抓本季新番 今天更新的動畫
+    } else if (msg === '!today') {
+      try {
+        const response1 = await axios.get('https://ani.gamer.com.tw/')
+        let $1 = cheerio.load(response1.data)
+        const links1 = []
+        const arr1Http = []
+        const arr1Img = []
+        const arr1Name = []
+        const arr1Ep = []
+        const arr1Date = []
+        const arr1Type = []
+        const arr1Com = []
+        const arr1Score = []
+        $1('.newanime-block .new-count-1 .anime-block a').each(function () {
+          // 動畫網址
+          $1(this).attr('href')
+          // console.log('https://ani.gamer.com.tw/' + $1(this).attr('href'))
+          arr1Http.push('https://ani.gamer.com.tw/' + $1(this).attr('href'))
+          // 動畫圖片
+          $1(this).find('.lazyload').attr('src')
+          // console.log($1(this).find('.lazyload').attr('src'))
+          arr1Img.push($1(this).find('.lazyload').attr('src'))
+          // 動畫名稱
+          $1(this).find('.anime-name p').eq(0).text()
+          // console.log($1(this).find('.anime-name p').eq(0).text())
+          arr1Name.push($1(this).find('.anime-name p').eq(0).text())
+          // 動畫目前集數
+          $1(this).find('.anime-episode p').text()
+          console.log($1(this).find('.anime-episode p').text())
+          arr1Ep.push($1(this).find('.anime-episode p').text())
+
+          links1.push('https://ani.gamer.com.tw/' + $1(this).attr('href'))
+        })
+        for (const link1 of links1) {
+          const response1 = await axios.get(link1)
+          $1 = cheerio.load(response1.data)
+          $1('.container-player').each(function () {
+            // 上架時間
+            $1('.anime_info_detail').find('p').text().substring(5, 100)
+
+            // 作品類型
+            $1('.data_type li').eq(0).text().substring(4, 100)
+
+            // 製作廠商
+            $1('.data_type li').eq(4).text().substring(4, 100)
+
+            // 評分
+            $1('.ACG-score').children().remove()
+            $1('.ACG-score').text()
+          })
+          arr1Date.push($1('.anime_info_detail').find('p').text().substring(5, 100))
+          arr1Type.push($1('.data_type li').eq(0).text().substring(4, 100))
+          arr1Com.push($1('.data_type li').eq(4).text().substring(4, 100))
+          arr1Score.push($1('.ACG-score').text())
+        }
+        // 使用 line flex message 的模板
+        const flex1 = {
+          type: 'flex',
+          altText: '這是 flex',
+          contents: {
+            type: 'carousel',
+            contents: []
+          }
+        }
+
+        // 跑迴圈將搜尋結果的每個動畫資料陣列 push 進 flex1 message 的 contents 裡面
+        for (let i = 0; i < arr1Name.length; i++) {
+          flex1.contents.contents.push({
+            // 動畫圖片
+            type: 'bubble',
+            size: 'micro',
+            hero: {
+              type: 'image',
+              url: `${arr1Img[i]}`,
+              size: 'full',
+              aspectMode: 'cover',
+              aspectRatio: '1:1',
+              // 點擊圖片跳到該動畫瘋網址
+              action: {
+                type: 'uri',
+                uri: `${arr1Http[i]}`
+              }
+            },
+
+            // 動畫名稱
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: `${arr1Name[i]}`,
+                  weight: 'bold',
+                  size: '14px',
+                  wrap: true
+                },
+
+                // 動畫評分
+                {
+                  type: 'box',
+                  layout: 'vertical',
+                  spacing: 'sm',
+                  margin: 'sm',
+                  contents: [
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      contents: [
+                        {
+                          type: 'icon',
+                          url: 'https://icons555.com/images/icons-red/image_icon_rating_pic_512x512.png',
+                          size: '15px'
+                        },
+                        {
+                          type: 'text',
+                          text: `${arr1Score[i]}`,
+                          size: '14px',
+                          weight: 'bold',
+                          margin: 'sm',
+                          offsetTop: '-2.5px',
+                          flex: 0
+                        }
+                      ]
+                    }
+                  ]
+                },
+
+                // 作品年份
+                {
+                  type: 'box',
+                  layout: 'vertical',
+                  margin: 'sm',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '上映時間',
+                          color: '#aaaaaa',
+                          size: '13px',
+                          flex: 0
+                        },
+                        {
+                          type: 'text',
+                          text: `${arr1Date[i]}`,
+                          wrap: true,
+                          size: '12px',
+                          offsetStart: '5px'
+                        }
+                      ]
+                    },
+
+                    // 作品類型
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '作品類型',
+                          color: '#aaaaaa',
+                          size: '13px',
+                          flex: 0
+                        },
+                        {
+                          type: 'text',
+                          text: `${arr1Type[i]}`,
+                          wrap: true,
+                          size: '12px',
+                          offsetStart: '5px'
+                        }
+                      ]
+                    },
+
+                    // 製作廠商
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '製作廠商',
+                          color: '#aaaaaa',
+                          size: '13px',
+                          flex: 0
+                        },
+                        {
+                          type: 'text',
+                          text: `${arr1Com[i]}`,
+                          wrap: true,
+                          size: '12px',
+                          offsetStart: '5px'
+                        }
+                      ]
+                    },
+                    // 最新集數
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '最新集數',
+                          color: '#aaaaaa',
+                          size: '13px',
+                          flex: 0
+                        },
+                        {
+                          type: 'text',
+                          text: `${arr1Ep[i]}`,
+                          wrap: true,
+                          size: '12px',
+                          offsetStart: '5px'
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ],
+              spacing: 'sm',
+              paddingAll: '10px'
+            }
+          })
+        }
+        if (arr1Name.length === 0) {
+          event.reply('查不到此關鍵字的資料\n請重新確認')
+        }
+        event.reply(flex1)
       } catch (error) {
         event.reply('發生錯誤')
       }
